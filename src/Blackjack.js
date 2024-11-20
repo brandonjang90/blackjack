46a3d6f880e7f6c84d0bee2e8f5419fee94e0532
Brandon Jang <brandonjang90@gmail.com>
2024-11-19 21:07:59 -0800

    finished

--- src/Blackjack.js
+++ src/Blackjack.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Cards from './Cards';
 
-// Card values mapping
 const CARD_VALUES = {
   'A': 11, // Ace
   '2': 2,
@@ -18,7 +17,6 @@ const CARD_VALUES = {
   'K': 10,
 };
 
-// All cards in a standard deck
 const DECK = [
   'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS',
   'AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH',
@@ -26,7 +24,6 @@ const DECK = [
   'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC',
 ];
 
-// Function to shuffle the deck and deal two cards
 const dealCards = () => {
   const shuffledDeck = [...DECK].sort(() => Math.random() - 0.5);
   const dealtCards = shuffledDeck.slice(0, 2);
